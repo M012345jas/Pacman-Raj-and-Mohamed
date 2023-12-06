de6ba9ee704bf9661f7d573ca8e6bebd7782016d
1100037496 <1100037496@L829WD210004.edu.cbe.ab.ca>
2023-12-06 10:54:22 -0700

    Added to node class

--- gui/Node.java
+++ gui/Node.java
@@ -1,6 +1,7 @@
-//import java.awt.Color;
-//import java.awt.Graphics2D;
-//import java.awt.geom.Line2D;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,5 +18,22 @@ public class Node {
 		neighbors.put("UP", null);
 		neighbors.put("DOWN", null);
 	}
-
+    public void render(Graphics2D g2d) {
+        for (String n : neighbors.keySet()) {
+            if (neighbors.get(n) != null) {
+                Point2D lineStart = position;
+                Point2D lineEnd = neighbors.get(n).getPosition();
+                g2d.setColor(Color.WHITE);
+                g2d.setStroke(new BasicStroke(4));
+                g2d.draw(new Line2D.Double(lineStart, lineEnd));
+                g2d.setColor(Color.RED);
+                g2d.fillOval((int) position.getX() - 6, (int) position.getY() - 6, 12, 12);
+            }
+        }
+    }
+	private Point2D getPosition() {
+		return position;
+	}
+	
+	
 }
