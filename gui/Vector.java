39b281b3de93c6019cd8e3be18fbda70842506ed
1100037496 <1100037496@L829WD210004.edu.cbe.ab.ca>
2023-12-11 11:11:25 -0700

    Just added a little to Vector class.

--- gui/Vector.java
+++ gui/Vector.java
@@ -4,4 +4,10 @@ public class Vector {
 	private double x;
 	private double y;
 	private double thresh;
+	
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
+		this.thresh = 0.000001;
+	}
 }
