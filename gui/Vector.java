106c9de99169832fcfeed6942561982867fa7cbf
1100037496 <1100037496@L829WD210004.edu.cbe.ab.ca>
2023-12-13 11:18:57 -0700

    added a little (method), to vector class.

--- gui/Vector.java
+++ gui/Vector.java
@@ -10,4 +10,15 @@ public class Vector {
 		this.y = y;
 		this.thresh = 0.000001;
 	}
+
+public Vector add(Vector other) {
+    return new Vector(this.x + other.x, this.y + other.y);
 }
+
+public Vector sub(Vector other) {
+    return new Vector(this.x - other.x, this.y - other.y);
+	}
+}
+
+
+
