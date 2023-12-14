8972988d4c773c0a044d409be570b855d01c5d3a
1100037496 <1100037496@L829WD210004.edu.cbe.ab.ca>
2023-12-14 09:57:23 -0700

    added to the vector class, will need to change or edit a method.

--- gui/Vector.java
+++ gui/Vector.java
@@ -11,14 +11,49 @@ public class Vector {
 		this.thresh = 0.000001;
 	}
 
-public Vector add(Vector other) {
-    return new Vector(this.x + other.x, this.y + other.y);
-}
-
-public Vector sub(Vector other) {
-    return new Vector(this.x - other.x, this.y - other.y);
+	public Vector add(Vector other) {
+		return new Vector(this.x + other.x, this.y + other.y);
 	}
-}
 
+	public Vector sub(Vector other) {
+		return new Vector(this.x - other.x, this.y - other.y);
+	}
+
+	public Vector neg() {
+		return new Vector(-this.x, -this.y);
+	}
+
+	public Vector mul(float scalar) {
+		return new Vector(this.x * scalar, this.y * scalar);
+	}
+
+	public Vector div(float scalar) {
+		if (scalar != 0) {
+			return new Vector(this.x / scalar, this.y / scalar);
+		}
+			return null;
+	}
+
+	public Vector truediv(float scalar) {
+		return div(scalar);
+	}
+	
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (other == null || getClass() != other.getClass()) {
+			return false;
+		}
+//		MyClass otherObj = (MyClass) other;						Need to make or change MyClass
+//		if (Math.abs(this.x - otherObj.x) < this.thresh) {
+//			if (Math.abs(this.y - otherObj.y) < this.thresh) {
+//	            return true;
+//			}
+//		}
+		return true;
+	}
+
+}	
 
 
